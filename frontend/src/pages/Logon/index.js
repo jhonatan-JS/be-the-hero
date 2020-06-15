c482e38d60820be1cc7225baa7bf8c6dc59687fc
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-06-14 20:51:08 -0400

    Page Profile

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -11,22 +11,22 @@ export default function Logon() {
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logoImg} alt="Be The Hero"/>
+                <img src={logoImg} alt="Be The Hero" />
 
                 <form>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <Link to="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
                     </Link>
                 </form>
             </section>
 
-            <img src={heroesImg} alt="Heroes"/>
+            <img src={heroesImg} alt="Heroes" />
         </div>
 
     );
